9f3ae80f56f7b823475618af5e9e8d491bf220d9
SAADEL9 <155107889+SAADEL9@users.noreply.github.com>
2025-10-16 20:57:47 +0100

    first

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+      source={require('./assets/emsi.png')}
+        style={styles.logo}
+        resizeMode="contain"
+        />
+     <Text style={styles.schoolName}>EMSI MAARIF</Text>
+     <View style={styles.info}>
+         <Text style={styles.label}>NOM :
+            <Text style={styles.value}> EL MAHI</Text>
+        </Text>
+ <Text style={styles.label}>PRENOM :
+            <Text style={styles.value}> SAAD</Text>
+        </Text>
+         <Text style={styles.label}>ANNEE UNIVERSITAIRE :
+            <Text style={styles.value}> 2025 / 2026</Text>
+        </Text>
+
+
+     </View>
+
+     
     </View>
   );
 }
@@ -13,8 +32,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f6f7fb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    width :200,
+    height :200
+  },
+  schoolName: {
+    fontSize : 22,
+    fontWeight :'bold',
+    color :'#1f2937',
+    marginTop : 20,
+    letterSpacing : 1,
+    flexDirection :'row',
+    justifyContent : 'space-around',
+
+  },
+  info :{
+    alignItems:'center',
+    gap : 8,
+  },
+  label :{
+    fontSize:16,
+    color : '#6b7280',
+    fontWeight:'500',
+  },
+   value: { 
+    fontSize: 17, 
+    color: '#111827', 
+    fontWeight: 'bold', 
+  }, 
 });
